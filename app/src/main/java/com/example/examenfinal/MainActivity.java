1136ac4118648a48da51247d52b7bf5d56b7abf8
Javier-Chamba <56860589+Javier-Chamba@users.noreply.github.com>
2020-02-12 16:53:35 -0500

    Initial commit

---
+++ app/src/main/java/com/example/examenfinal/MainActivity.java
@@ -0,0 +1,200 @@
+package com.example.examenfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.services.vision.v1.Vision;
+import com.google.api.services.vision.v1.VisionRequestInitializer;
+import com.google.api.services.vision.v1.model.AnnotateImageRequest;
+import com.google.api.services.vision.v1.model.AnnotateImageResponse;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesRequest;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesResponse;
+import com.google.api.services.vision.v1.model.Feature;
+import com.google.api.services.vision.v1.model.Image;
+import com.google.api.services.vision.v1.model.ImageSource;
+import com.google.api.services.vision.v1.model.TextAnnotation;
+import com.google.api.services.vision.v1.model.WebDetection;
+
+import org.apache.commons.io.output.ByteArrayOutputStream;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private ImageView mPhotoImageView;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    public Vision vision;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mPhotoImageView = findViewById(R.id.imageView);
+
+        Vision.Builder visionBuilder = new Vision.Builder(new NetHttpTransport(),
+                new AndroidJsonFactory(),  null);
+        visionBuilder.setVisionRequestInitializer(new VisionRequestInitializer("AIzaSyCV_ADpVQp5_K1CQ98gc6KeOVq5p1sjqKQ"));
+        vision = visionBuilder.build();
+
+        checkCameraPermission();
+
+
+
+    }
+
+
+
+    public void btnPais(View view){
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                ImageView imagen=(ImageView)findViewById(R.id.imageView);
+                BitmapDrawable drawable = (BitmapDrawable) imagen.getDrawable();
+                Bitmap bitmap = drawable.getBitmap();
+
+                bitmap = scaleBitmapDown(bitmap, 1200);
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 90, stream);
+                byte[] imageInByte = stream.toByteArray();
+
+                //1.Paso
+                Image inputImage = new Image();
+                inputImage.encodeContent(imageInByte);
+
+                //2.Feature
+                Feature desiredFeature = new Feature();
+                desiredFeature.setType("WEB_DETECTION");
+
+                //3.Arma la solicitud(es)
+                AnnotateImageRequest request = new AnnotateImageRequest();
+                request.setImage(inputImage);
+                request.setFeatures(Arrays.asList(desiredFeature));
+                BatchAnnotateImagesRequest batchRequest =  new BatchAnnotateImagesRequest();
+                batchRequest.setRequests(Arrays.asList(request));
+
+
+                //4. Asignamos al control VisionBuilder la solicitud
+                try {
+                    Vision.Images.Annotate annotateRequest  = vision.images().annotate(batchRequest);
+
+                    //5. Enviamos la solicitud y obtenemos la respuesta
+                    annotateRequest.setDisableGZipContent(true);
+                    BatchAnnotateImagesResponse batchResponse  = annotateRequest.execute();
+
+
+                    //6. Obtener la respuesta
+                    TextAnnotation text = batchResponse.getResponses().get(0).getFullTextAnnotation();
+                    String message="";
+                    if(text!=null){
+                        message=text.getText();
+                    }else{
+                        message="No hay texto";
+                    }
+                    final String result = message;
+
+                    //Asignar la respuesta a la UI
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            TextView imageDetail = (TextView)findViewById(R.id.textView);
+                            imageDetail.setText(result);
+                        }
+                    });
+
+                }catch(IOException e) {
+                    e.getStackTrace();
+                }
+
+
+
+
+
+            }
+        });
+
+    }
+
+
+
+    public void btnCamera(View v){
+        Intent takePictureIntent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager())!=null){
+            startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras(); // Aquí es null
+            Bitmap bitmap = (Bitmap) extras.get("data");
+            mPhotoImageView.setImageBitmap(bitmap);
+        }
+
+
+    }
+
+
+    private void checkCameraPermission() {
+        int permissionCheck = ContextCompat.checkSelfPermission(
+                this, Manifest.permission.CAMERA);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            Log.i("Mensaje", "No se tiene permiso para la camara!.");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 225);
+        } else {
+            Log.i("Mensaje", "Tienes permiso para usar la camara.");
+        }
+    }
+
+    private Bitmap scaleBitmapDown(Bitmap bitmap, int maxDimension) {
+        int originalWidth = bitmap.getWidth();
+        int originalHeight = bitmap.getHeight();
+        int resizedWidth = maxDimension;
+        int resizedHeight = maxDimension;
+        if (originalHeight > originalWidth) {
+            resizedHeight = maxDimension;
+            resizedWidth = (int) (resizedHeight * (float) originalWidth / (float) originalHeight);
+        } else if (originalWidth > originalHeight) {
+            resizedWidth = maxDimension;
+            resizedHeight = (int) (resizedWidth * (float) originalHeight / (float) originalWidth);
+        } else if (originalHeight == originalWidth) {
+            resizedHeight = maxDimension;
+            resizedWidth = maxDimension;
+        }
+        return Bitmap.createScaledBitmap(bitmap, resizedWidth, resizedHeight, false);
+    }
+
+
+
+}
